d4c33fbc7dc7175f5ca4691317cf795786d88fea
ZhenchenXu <78614291+ZhenchenXu@users.noreply.github.com>
2021-02-26 21:54:23 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -50,148 +50,156 @@ while (true) {
     }
 }
 basic.forever(function () {
-    if (T == 1) {
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
-            . . . . .
-            `)
-        E = 1
-        T = 0
-    }
-    if (T == 2) {
-        basic.showLeds(`
-            # . # . #
-            . # . # .
-            # . # . #
-            . # . # .
-            # . # . #
-            `)
-        E = 1
-        T = 0
-    }
-    if (T == 3) {
-        basic.showLeds(`
-            . . . . .
-            # . # # #
-            . # . . .
-            # . # # #
-            . . . . .
-            `)
-        E = 1
-        T = 0
-    }
-})
-basic.forever(function () {
-    if (E == 1) {
-        basic.showString("E")
-        E = 0
-        E = randint(2, 4)
+    while (true) {
+        if (T == 1) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+            E = 1
+            T = 0
+        }
+        if (T == 2) {
+            basic.showLeds(`
+                # . # . #
+                . # . # .
+                # . # . #
+                . # . # .
+                # . # . #
+                `)
+            E = 1
+            T = 0
+        }
+        if (T == 3) {
+            basic.showLeds(`
+                . . . . .
+                # . # # #
+                . # . . .
+                # . # # #
+                . . . . .
+                `)
+            E = 1
+            T = 0
+        }
     }
 })
 basic.forever(function () {
-    if (E == 2) {
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
-            . . . . .
-            `)
-    }
-    if (E == 3) {
-        basic.showLeds(`
-            # . # . #
-            . # . # .
-            # . # . #
-            . # . # .
-            # . # . #
-            `)
-    }
-    if (E == 4) {
-        basic.showLeds(`
-            . . . . .
-            # . # # #
-            . # . . .
-            # . # # #
-            . . . . .
-            `)
+    while (true) {
+        if (E == 1) {
+            basic.showString("E")
+            E = 0
+            E = randint(2, 4)
+        }
     }
 })
 basic.forever(function () {
-    if (E == 2 && T == 1) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # # # # #
-            . . . . .
-            `)
-    } else if (E == 2 && T == 2) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            . # # # .
-            # . . . #
-            `)
-    } else if (E == 2 && T == 3) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-    } else if (E == 3 && T == 1) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-    } else if (E == 3 && T == 2) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # # # # #
-            . . . . .
-            `)
-    } else if (E == 3 && T == 3) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            . # # # .
-            # . . . #
-            `)
-    } else if (E == 4 && T == 1) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            . # # # .
-            # . . . #
-            `)
-    } else if (E == 4 && T == 2) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-    } else if (E == 4 && T == 3) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # # # # #
-            . . . . .
-            `)
+    while (true) {
+        if (E == 2) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+        }
+        if (E == 3) {
+            basic.showLeds(`
+                # . # . #
+                . # . # .
+                # . # . #
+                . # . # .
+                # . # . #
+                `)
+        }
+        if (E == 4) {
+            basic.showLeds(`
+                . . . . .
+                # . # # #
+                . # . . .
+                # . # # #
+                . . . . .
+                `)
+        }
+    }
+})
+basic.forever(function () {
+    while (true) {
+        if (E == 2 && T == 1) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # # # # #
+                . . . . .
+                `)
+        } else if (E == 2 && T == 2) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                . # # # .
+                # . . . #
+                `)
+        } else if (E == 2 && T == 3) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+        } else if (E == 3 && T == 1) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+        } else if (E == 3 && T == 2) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # # # # #
+                . . . . .
+                `)
+        } else if (E == 3 && T == 3) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                . # # # .
+                # . . . #
+                `)
+        } else if (E == 4 && T == 1) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                . # # # .
+                # . . . #
+                `)
+        } else if (E == 4 && T == 2) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+        } else if (E == 4 && T == 3) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # # # # #
+                . . . . .
+                `)
+        }
     }
 })
