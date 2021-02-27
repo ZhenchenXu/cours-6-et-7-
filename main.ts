c4e47cd8c0a1a52e18430c666742b4e094a51181
ZhenchenXu <78614291+ZhenchenXu@users.noreply.github.com>
2021-02-26 21:02:55 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let E = 0
 let T = 0
 basic.showString("Bienvenue au roche papier ciseau, t=toi, et e=l'ennemi.  ")
 basic.showLeds(`
@@ -39,7 +40,96 @@ while (true) {
         basic.showString("T")
         T = 1
     }
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showString("T")
+        T = 2
+    }
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.showString("T")
+        T = 3
+    }
 }
 basic.forever(function () {
-	
+    if (T == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        E = 1
+        T = 0
+    }
+    if (T == 2) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+        E = 1
+        T = 0
+    }
+    if (T == 3) {
+        basic.showLeds(`
+            . . . . .
+            # . # # #
+            . # . . .
+            # . # # #
+            . . . . .
+            `)
+        E = 1
+        T = 0
+    }
+})
+basic.forever(function () {
+    if (E == 1) {
+        basic.showString("E")
+        E = 0
+        E = randint(2, 4)
+    }
+})
+basic.forever(function () {
+    if (E == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (E == 3) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+    }
+    if (E == 4) {
+        basic.showLeds(`
+            . . . . .
+            # . # # #
+            . # . . .
+            # . # # #
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (true) {
+    	
+    } else if (0 == 0 && 0 == 0) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
 })
